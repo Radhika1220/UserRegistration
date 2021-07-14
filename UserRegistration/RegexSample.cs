142b3598e3bea634fc427b8df5f406f80bad524e
Radhika <radhika.shankar1220@gmail.com>
2021-07-14 11:45:11 +0530

    [Radhika] Atleast one upper case letter in a string

--- UserRegistration/RegexSample.cs
+++ UserRegistration/RegexSample.cs
@@ -102,8 +102,8 @@ namespace UserRegistration
 
         public void ValidatingPassWord()
         {
-            string[] phoneNumInput = {"Radhika12","Sumathi32","Priya"};
-            string passwordPattern = @"[a-z,A-Z,0-9]{8,}$";
+            string[] phoneNumInput = {"Radhika12","sumAthi32","Priya","praveena"};
+            string passwordPattern = @"^(?=.*[A-Z]).{8,}$";
             Regex regex = new Regex(passwordPattern);
             for (int i = 0; i < phoneNumInput.Length; i++)
             {
