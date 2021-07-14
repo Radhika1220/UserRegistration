eab948e7ef1e6b6e4939ee24af6f30f7ac5f7dc0
Radhika <radhika.shankar1220@gmail.com>
2021-07-14 10:59:24 +0530

    [Radhika] Validating email id

--- UserRegistration/RegexSample.cs
+++ UserRegistration/RegexSample.cs
@@ -14,6 +14,8 @@ namespace UserRegistration
             ValidatingFirstName();
             Console.WriteLine("Validating The Last Name");
             ValidatingLastName();
+            Console.WriteLine("Validating The Email Id");
+            ValidatingEmailId();
         }
         public void ValidatingFirstName()
         {
@@ -45,5 +47,27 @@ namespace UserRegistration
                 Console.WriteLine("Invalid");
             }
         }
+        public void ValidatingEmailId()
+        {
+            string[] emailInput = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",  "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com",  "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+            string emailPattern= (@"^[a-zA-Z0-9]+([\.\+\-][a-zA-Z0-9]+)?@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,}(\.[a-zA-Z]+)?)$");
+            Regex regex = new Regex(emailPattern);
+            for (int i = 0; i < emailInput.Length; i++)
+            {
+                Match match = regex.Match(emailInput[i]);
+                if (match.Success)
+                {
+                    Console.WriteLine(emailInput[i] + " ----->Valid");
+                }
+                else
+                {
+                    Console.WriteLine(emailInput[i] + " ----->Invalid");
+                }
+            }
+
+        }
+
+
+
     }
 }
